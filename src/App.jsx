a67d8b696ba32546dff5dfb50988739b1128654c
rioto <rioto@students.cs.ubc.ca>
2023-11-19 19:14:29 -0800

    works

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Header from "./Header/Header";
 import OnButton from "./OnButton/OnButton";
 import Readability from "./Readability/Readability";
 import Settings from "./Settings/Settings";
-// import OnButton from 
+// import OnButton from
 // import Readability from "./Readability";
 
 const App = () => {
   return (
-  <Router>
-    <div>
-      {/* start of header */}
+    <Router>
+      <div>
+        {/* start of header */}
         <Header />
-        
+
         {/* start of on button */}
         <OnButton />
-        
+
         {/* start of readability */}
         <Readability />
 
         {/* <Link to="/settings">settings</Link> */}
 
         <Routes>
-          <Route path="/settings" element={<Settings/>}/>
+          <Route path="/settings" element={<Settings />} />
         </Routes>
-    </div>
-  </Router>
+      </div>
+    </Router>
   );
-}
-
+};
 
 export default App;
--- src/OnButton/OnButton.jsx
+++ src/OnButton/OnButton.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { ReactDOM } from "react";
 import "./OnButton.css";
+import handleChangerClick from "../Settings/Settings";
 
 const OnButton = () => {
-
- return (
-  <div className="onButton">
-    <p className="onButtonText">on</p>
-    <label class="switch">
-    <input type="checkbox"/>
-    <span class="slider"></span>
-    </label>
+  return (
+    <div className="onButton">
+      <p className="onButtonText">on</p>
+      <label class="switch">
+        <input type="checkbox" />
+        <span class="slider" onClick={handleChangerClick}></span>
+      </label>
     </div>
- )
-}
+  );
+};
 
 export default OnButton;
