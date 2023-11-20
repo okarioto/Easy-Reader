c2769eb623e3f17075d216af0a295c0194649f32
leebr <leebrian1239@gmail.com>
2023-11-19 19:23:28 -0800

    m

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
   return (
   <div>
     <header className="homeHeader"> 
-      <img className="homeLogo" src="./images/glassesT.png" alt="glasses" /> 
-        <h3 className="title">title</h3>
+      <img className="homeLogo" src="./images/HackCamp-project-logo.png" alt="glasses" /> 
+        <h3 className="title">easy reader</h3>
         <Link to="/settings">
           <img className="homeSettingsLogo" src="./images/settingsT.png" alt="settings" />
         </Link>
